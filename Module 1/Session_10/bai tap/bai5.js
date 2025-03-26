937fca871cb402e63fff021f3bed59bb05baf798
HOANG <minhhoang3320@gmail.com>
2025-03-26 21:51:34 +0900

    Add files via upload

---
+++ Module 1/Session_10/bai tap/bai5.js
@@ -0,0 +1,5 @@
+let inputUsd = + prompt("Nhập số tiền Đô la Mỹ bạn muốn đổi: ");
+
+let outVnd = inputUsd * 25000;
+
+document.write(`<h1>${inputUsd} Đô la Mỹ bằng ${outVnd} đ</h1>`)
---
+++ Module 1/Session_10/bai tap/bai6.js
@@ -0,0 +1,8 @@
+
+let math = 8;
+let physics = 9;
+let chemistry = 7;
+
+let averageScore = (math + physics + chemistry)/3;
+
+document.write(`<h1>Điểm trung bình là : ${averageScore}</h1>`)
